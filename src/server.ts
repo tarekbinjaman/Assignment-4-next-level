d081414bf4cc05ff62bd253ac7aab636d0ec887b
tarekbinjaman <tarekbinjaman@gmail.com>
2026-04-09 03:21:36 +0600

    server initial setup done

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import app from "./app";
+import { prisma } from "./lib/prisma";
+
+const PORT = process.env.PORT || 5000;
+
+async function main() {
+  try {
+    await prisma.$connect();
+    console.log("Connected to the database successfully");
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("An error occured", error);
+    prisma.$disconnect();
+    process.exit(1);
+  }
+}
+
+main();
