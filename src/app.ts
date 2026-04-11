9f9269bfa4a42cbd7b6964260972eafe45b07c6d
tarekbinjaman <tarekbinjaman@gmail.com>
2026-04-12 03:33:36 +0600

    user.routes.ts file created and used in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express'
+import userRoutes from './module/user/user.route'
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
     res.send("Hello, World")
---
+++ src/module/user/user.route.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import * as UserController from "./user.controller";
+
+const router = express.Router();
+
+router.post("/", UserController.createUser);
+router.get("/", UserController.getAllUser);
+router.get("/:id", UserController.getSingleUser);
+router.delete("/:id", UserController.deleteUser);
+
+export default router;
