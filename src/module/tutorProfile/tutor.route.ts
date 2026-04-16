bbec4f25592d5558f4ae6d03ad010a435087b6f8
tarekbinjaman <tarekbinjaman@gmail.com>
2026-04-17 01:05:22 +0600

    tutor module route done

---
+++ src/module/tutorProfile/tutor.route.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import * as TutorController from "./tutor.controller";
+import { authenticateUser } from "../auth/auth.authenticate";
+
+const router = express.Router();
+
+router.post("/", authenticateUser, TutorController.createTutor);
+router.get("/", TutorController.getTutors);
+router.get("/:id", TutorController.getTutor);
+router.patch("/:id", authenticateUser, TutorController.updateTutor);
+
+export default router;
