2dd32897a195fb7024f3607466866b2a0a94232e
tarekbinjaman <tarekbinjaman@gmail.com>
2026-05-02 23:22:40 +0600

    gemt logic added

--- src/module/user/user.controller.ts
+++ src/module/user/user.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import * as UserService from "./user.service";
+import { prisma } from "../../lib/prisma";
 
 export const createUser = async (req: Request, res: Response) => {
   try {
@@ -60,3 +61,35 @@ export const deleteUser = async (req: Request, res: Response) => {
     });
   }
 };
+
+export const getMe = async (req: Request, res: Response) => {
+  try {
+    const userId = (req as any).user?.userId;
+    if(!userId) {
+      return res.status(400).json({success: false, message: "User Id not found in token."});
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {id: userId},
+      select: {
+        id: true,
+        email: true,
+        role: true,
+        name: true,
+        image: true,
+      }
+    });
+
+    if(!user) {
+      return res.status(404).json({success: false, message: "User not found."});
+    }
+
+    res.status(200).json({success: true, data: user});
+
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    })
+  }
+}
