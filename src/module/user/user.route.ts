80b7df1cfe07ba0ec58c6953d29be47ae4d4e6b0
tarekbinjaman <tarekbinjaman@gmail.com>
2026-05-02 23:29:00 +0600

    /me route complete in user.route.ts

--- src/module/user/user.route.ts
+++ src/module/user/user.route.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import * as UserController from "./user.controller";
+import { authenticateUser } from "../../middleware/authenticate";
 
 const router = express.Router();
 
 router.post("/register", UserController.createUser);
 router.get("/", UserController.getAllUser);
 router.get("/:id", UserController.getSingleUser);
+router.get("/me", authenticateUser, UserController.getMe);
 router.delete("/:id", UserController.deleteUser);
 
 export default router;
