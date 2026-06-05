3763ee0038a661bc03763eb0468f767db502d7f3
tarekbinjaman <tarekbinjaman@gmail.com>
2026-06-06 05:51:50 +0600

    availability update service created

--- src/module/availability/availability.service.ts
+++ src/module/availability/availability.service.ts
@@ -31,3 +31,33 @@ export const getMyAvailability = async (userId: string) => {
 };
 
 
+export const updateAvailability = async (userId: string, availabilityId: string, payload: any) => {
+    const tutor = await prisma.tutorProfile.findUnique({
+        where: {userId},
+    });
+
+    if(!tutor) {
+        throw new Error("Tutor not found");
+    }
+
+    const availability = await prisma.availability.findUnique({
+        where: {id: availabilityId}
+    })
+
+    if(!availability) {
+        throw new Error("Availability not found");
+    }
+    if(availability?.tutorId !== tutor?.id) {
+        throw new Error("Unauthorized");
+    }
+    return await prisma.availability.update({
+        where: {
+            id: availabilityId,
+        },
+        data: {
+            day: payload.day,
+            startTime: payload.startTime,
+            endTime: payload.endTime,
+        },
+    });
+};
