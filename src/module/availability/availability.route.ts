20f3abab61b7150f5d85177395bb48234fddd2c3
tarekbinjaman <tarekbinjaman@gmail.com>
2026-06-06 06:25:19 +0600

    availability update and delte route created

--- src/module/availability/availability.route.ts
+++ src/module/availability/availability.route.ts
@@ -6,5 +6,7 @@ const router = express.Router();
 
 router.post("/", authenticateUser, AvailabilityController.createAvailAbility);
 router.get("/", authenticateUser, AvailabilityController.getMyAvailability);
+router.patch("/:id", authenticateUser, AvailabilityController.updateAvailability);
+router.delete("/:id", authenticateUser, AvailabilityController.deleteAvailability);
 
 export default router;
