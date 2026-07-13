a26ffff8b7cf2fc975ee08c7c0a0601bdb60b135
tarekbinjaman <tarekbinjaman@gmail.com>
2026-07-13 20:08:45 +0600

    backend updated

--- src/module/dashboard/dashboard.sevice.ts
+++ src/module/dashboard/dashboard.sevice.ts
@@ -73,61 +73,79 @@ const getStudentDashboard = async (studentId: string) => {
 
   const recentBookings = await prisma.booking.findMany({
     where: {
-        studentId,
+      studentId,
     },
     orderBy: {
-        createdAt: "desc",
+      createdAt: "desc",
     },
     take: 5,
     include: {
-        tutor: {
-            include: {
-                user: true,
-                categories: true,
-            },
+      tutor: {
+        include: {
+          user: true,
+          categories: true,
         },
+      },
+    },
+  });
+
+  // =========================
+  // Total sessions including (pending, accepted, completed)
+  // =========================
+
+  const totalSessions = await prisma.booking.count({
+    where: {
+      studentId,
+      status: {
+        in: [
+          BookingStatus.PENDING,
+          BookingStatus.ACCEPTED,
+          BookingStatus.COMPLETED,
+        ],
+      },
     },
   });
 
   return {
     stats: {
-        upcomingSessions,
-        completedSessions,
-        totalSpent: Number(totalSpentResult._sum.totalPrice ?? 0),
-        reviewsGiven,
+      upcomingSessions,
+      completedSessions,
+      totalSpent: Number(totalSpentResult._sum.totalPrice ?? 0),
+      reviewsGiven,
     },
-    nextSession: nextSession ? {
-        id: nextSession.id,
-        tutorName: nextSession.tutor.user.name,
-        tutorImage: nextSession.tutor.user.image,
-        category: 
-        nextSession.tutor.categories.length > 0
-        ? nextSession.tutor.categories[0]?.name
-        : "General",
-        date: nextSession.date,
-        startTime: nextSession.startTime,
-        endTime: nextSession.endTime,
-        status: nextSession.status,
-    }
-    : null,
+    nextSession: nextSession
+      ? {
+          id: nextSession.id,
+          tutorName: nextSession.tutor.user.name,
+          tutorImage: nextSession.tutor.user.image,
+          category:
+            nextSession.tutor.categories.length > 0
+              ? nextSession.tutor.categories[0]?.name
+              : "General",
+          date: nextSession.date,
+          startTime: nextSession.startTime,
+          endTime: nextSession.endTime,
+          status: nextSession.status,
+        }
+      : null,
 
     recentBookings: recentBookings.map((booking) => ({
-        id: booking.id,
-        tutorName: booking.tutor.user.name,
-        tutorImage: booking.tutor.user.image,
-        category: 
+      id: booking.id,
+      tutorName: booking.tutor.user.name,
+      tutorImage: booking.tutor.user.image,
+      category:
         booking.tutor.categories.length > 0
-        ? booking.tutor.categories[0]?.name
-        : "General",
-        date: booking.date,
-        startTime: booking.startTime,
-        endTime: booking.endTime,
-        status: booking.status,
+          ? booking.tutor.categories[0]?.name
+          : "General",
+      date: booking.date,
+      startTime: booking.startTime,
+      endTime: booking.endTime,
+      status: booking.status,
     })),
+    totalSessions,
   };
 };
 
-
 export const DashboardService = {
-    getStudentDashboard,
+  getStudentDashboard,
 };
